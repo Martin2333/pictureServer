b1c70eea320883c23c0c87e5e550d46781120315
Martin2333 <parkhunt@163.com>
2017-11-28 09:37:33 +0800

    test1：（新增分支）新增导入图片界面

--- src/main/java/com/topsports/pictureServer/controller/MainController.java
+++ src/main/java/com/topsports/pictureServer/controller/MainController.java
@@ -1,16 +1,29 @@
 package com.topsports.pictureServer.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 /**
  * Created by huang.cj on 2017/10/30.
  */
 @Controller
 public class MainController {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @RequestMapping("login")
     public String login(){
         return "login";
+    }
+
+    @RequestMapping("")
+    public String defaul(){
+        return "index";
+    }
+    @RequestMapping("index")
+    public String index(){
+        return "index";
     }
 }
--- src/main/java/com/topsports/pictureServer/controller/UploadFilesController.java
+++ src/main/java/com/topsports/pictureServer/controller/UploadFilesController.java
@@ -1,10 +1,20 @@
 package com.topsports.pictureServer.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by huang.cj on 2017/10/30.
  */
 @Controller
+@RequestMapping(value = "uploadPicture")
 public class UploadFilesController {
+    private static final Logger logger = LoggerFactory.getLogger(UploadFilesController.class);
+    @RequestMapping(value="/uploadPicture")
+    public String uploadPictures(Model model){
+        return "uploadPicture";
+    }
 }
